2acd262060ac2999ae1a7fe67a2dd4af8d39e109
Samrocks58 <60971882+Samrocks58@users.noreply.github.com>
2025-03-30 07:48:48 -0400

    update

--- routes/indeed_scraper_routes.py
+++ routes/indeed_scraper_routes.py
@@ -1,7 +1,7 @@
 import imp
 from fastapi import APIRouter, Depends
 from indeed_scraper.indeed_scraper import start_indeed_scraper
-from dependencies import get_api_key
+from dependencies import get_indeed_api_key
 from pydantic import BaseModel
 
 router = APIRouter()
@@ -11,6 +11,6 @@ class JobSearchRequest(BaseModel):
     position: str
 
 @router.post("/scrape")
-def scrape_indeed(request: JobSearchRequest, api_key: str = Depends(get_api_key)):
+def scrape_indeed(request: JobSearchRequest, api_key: str = Depends(get_indeed_api_key)):
     result = start_indeed_scraper(request.location, request.position, api_key)
     return {"message": "Scraping started", "result": result}
